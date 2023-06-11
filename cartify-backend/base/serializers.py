b4c60ecfa924db596db3296ab19aa9dd6a1c70c8
progIfe <ifeoluwasamson90@gmail.com>
2023-06-11 20:59:25 +0100

    JWT Authentication Setup

--- cartify-backend/base/serializers.py
+++ cartify-backend/base/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, RefreshToken
+
 from django.contrib.auth.models import User
 from .models import Product
 
@@ -7,3 +9,16 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+        
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        data = super().validate(attrs)
+
+        serializer = UserSerializerwithToken(self.user).data
+        for key, value in serializer.items():
+            data[key] = value
+
+        return data
+        
+        
---
+++ cartify-backend/base/views/user_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import status
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
