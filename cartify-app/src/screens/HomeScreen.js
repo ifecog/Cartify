886fadb454632d6b2afe17968883480c007e36bf
progIfe <ifeoluwasamson90@gmail.com>
2023-06-11 12:51:12 +0100

    Configuration of Message and Loader components

--- cartify-app/src/screens/HomeScreen.js
+++ cartify-app/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from 'react-redux'
 import {useNavigate, useLocation} from 'react-router-dom'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import {listProducts} from '../actions/productActions'
 
 function HomeScreen() {
@@ -20,15 +22,21 @@ function HomeScreen() {
   return (
     <div>
       <h1>Latest Products</h1>
-      <div>
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      </div>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <div>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </div>
+      )}
     </div>
   )
 }
