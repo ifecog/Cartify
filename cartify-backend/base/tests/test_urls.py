b4cdf09980d5d647f1282366e1dca0a0e3a9d0e7
progIfe <ifeoluwasamson90@gmail.com>
2023-06-12 07:15:46 +0100

    user urls Unittest

--- cartify-backend/base/tests/test_urls.py
+++ cartify-backend/base/tests/test_urls.py
@@ -4,6 +4,11 @@ from base.views.product_views import (
     get_products,
     get_product_details,
 )
+from base.views.user_views import (
+    get_users,
+    MyTokenObtainPairView,
+    get_user_profile,
+)
 
 class TestProductUrls(SimpleTestCase):
     
@@ -14,4 +19,19 @@ class TestProductUrls(SimpleTestCase):
     def test_product_details_url_is_resolved(self):
         url = reverse('product-details', args=['some-slug'])
         self.assertEquals(resolve(url).func, get_product_details)
+        
+
+class TestUserUrls(SimpleTestCase):
+    
+    def test_users_url_is_resolved(self):
+        url = reverse('users')
+        self.assertEquals(resolve(url).func, get_users)
+        
+    def test_login_user_is_resolved(self):
+        url = reverse('token-obtain-pair')
+        self.assertEquals(resolve(url).func.view_class, MyTokenObtainPairView)
+
+    def test_user_profile_url_is_resolved(self):
+        url = reverse('user-profile')
+        self.assertEquals(resolve(url).func, get_user_profile)
 
--- cartify-backend/base/urls/user_urls.py
+++ cartify-backend/base/urls/user_urls.py
@@ -3,6 +3,6 @@ from base.views import user_views as views
 
 urlpatterns = [
     path('', views.get_users, name='users'),
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('profile/', views.get_user_profile, name='user-profile'),
 ]
