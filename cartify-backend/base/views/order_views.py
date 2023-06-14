5c78f2e9ab677d65b0aa87a7649e5445e65d85d1
progIfe <ifeoluwasamson90@gmail.com>
2023-06-14 22:28:33 +0100

    Order View & URL

---
+++ cartify-backend/base/views/order_views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import OrderSerializer
+
+from rest_framework import status
+
+from datetime import datetime
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_order_items(request):
+    user = request.user
+    data = request.data
+    
+    orderItems = data['orderItems']
+    
+    if orderItems and len(orderItems) == 0:
+        message = {'detail': 'No Order Items'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+    else:
+        # 1. create order
+        order = Order.objects.create(
+            user=user,
+            payment_method=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+        
+        # 2. create shipping address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            country=data['shippingAddress']['country'],
+            city=data['shippingAddress']['city'],
+            address=data['shippingAddress']['address'],
+            postalCode=data['shippingAddress']['postalCode']
+        )
+        
+        # 3. create order items and set order to orderItem relationship
+        for i in orderItems:
+            for i in orderItems:
+                product = Product.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url
+            )
+
+            # 4. update stock
+            product.count_in_stock -= item.qty
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+
+        return Response(serializer.data)
