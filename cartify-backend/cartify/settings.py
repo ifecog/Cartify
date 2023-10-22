33e584110525ded2d9253d3ec7abe787f8443738
progIfe <ifeoluwasamson90@gmail.com>
2023-10-22 13:40:48 +0100

    Added drf-spectacular and setup schema.yml

--- cartify-backend/cartify/settings.py
+++ cartify-backend/cartify/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'drf_spectacular',
     'corsheaders',
     'storages',
 ]
@@ -213,3 +214,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Cartify API',
+    'DESCRIPTION': 'Cartify API is a fully functional, robust and secure Ecommerce RESTful API built for the Cartify mobile and web app.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
